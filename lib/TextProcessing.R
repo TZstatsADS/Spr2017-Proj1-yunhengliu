66678663ba8dcb1b5db7881031a44e9412d4f15a
Ethan Liu <yunhengliu@gmail.com>
2017-02-02 20:45:56 -0500

    added code for topic modeling

--- lib/TextProcessing.R
+++ lib/TextProcessing.R
@@ -1,10 +1,15 @@
-getSpeeches <- function(folder.path){
+getSpeeches <- function(folder.path, extra.words=list()){
   docs <- Corpus(DirSource(folder.path))   
   docs <- tm_map(docs, stripWhitespace)
   docs <- tm_map(docs, content_transformer(tolower))
+  docs <- tm_map(docs, removeNumbers)
   docs <- tm_map(docs, removeWords, stopwords("english"))
+  if(length(extra.words)>0){
+    docs <- tm_map(docs, removeWords, extra.words)
+  }
   docs <- tm_map(docs, removePunctuation)
   docs <- tm_map(docs, removeWords, character(0))
+  docs <- tm_map(docs, stemDocument)
   return(docs)
 }
 
